ad1cd427df3be7077c59621e73bdba7b51bf270e
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2023-10-30 10:29:14 +0100

    finalized sheet 1

---
+++ excercises/01.R
@@ -0,0 +1,82 @@
+## ----Aufgabe 3-----------------------------------------
+data <- wooldridge::cps78_85
+
+
+## ----Aufgabe 4-----------------------------------------
+data$wage <- exp(data$lwage)
+
+
+## ----Aufgabe 6-----------------------------------------
+str(data)
+
+plot(
+  data$educ[data$y85 == 0], 
+  data$wage[data$y85 == 0], 
+  main = "Zusammenhang Ausbildung und Gehalt in 1978 vs. 1985", 
+  xlab = "educ (Ausbildungszeit in Jahren)", 
+  ylab = "wage (Stundenlohn in USD)",
+  xlim = c(0, max(data$educ)),
+  ylim = c(0, max(data$wage))
+)
+points(
+  data$educ[data$y85 == 1] + 0.15, 
+  data$wage[data$y85 == 1], 
+  col = "red"
+)
+legend(
+  "topleft", 
+  legend = c("1978", "1985"), 
+  col = c(1:2), 
+   pch = "o"
+)
+
+
+## ----Aufgabe 7-----------------------------------------
+model <- lm(formula = wage ~ educ, data = data)
+summary(model)
+
+
+## ----Aufgabe 8-----------------------------------------
+plot(
+  model$fitted, model$residuals,
+  xlab = "angepasste Werte",
+  ylab = "Residuen"
+)
+abline(h = 0)
+
+breusch_pagan <- lm(formula = I(model$residuals^2) ~ data$educ)
+summary(breusch_pagan)
+
+white <- lm(formula = I(model$residuals^2) ~ model$fitted + I(model$fitted^2))
+summary(white)
+
+
+## ----Aufgabe 10----------------------------------------
+model_log <- lm(formula = lwage ~ educ, data = data)
+summary(model_log)
+
+plot(
+  model_log$fitted, model_log$residuals,
+  xlab = "angepasste Werte (log Modell)",
+  ylab = "Residuen (log Modell)"
+)
+abline(h = 0)
+
+breusch_pagan <- lm(formula = I(model_log$residuals^2) ~ data$educ)
+summary(breusch_pagan)
+
+white <- lm(formula = I(model_log$residuals^2) ~ model_log$fitted + I(model_log$fitted^2))
+summary(white)
+
+
+## ----Aufgabe 11----------------------------------------
+model_log_dummy <- lm(formula = lwage ~ educ + y85, data = data)
+summary(model_log_dummy)
+
+
+## ----Aufgabe 12----------------------------------------
+model_log_dummy_interact <- lm(
+  formula = lwage ~ educ + y85 + I(y85 * educ), data = data
+)
+summary(model_log_dummy_interact)
+
