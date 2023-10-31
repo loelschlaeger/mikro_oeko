bdd98ceefdc035f461f83f87c8192c1ec2abc660
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2023-10-31 14:04:26 +0100

    two more exercises for sheet 1

--- excercises/01.R
+++ excercises/01.R
@@ -1,12 +1,12 @@
-## ----Aufgabe 3-----------------------------------------
+## ----Aufgabe 3------------------------------------
 data <- wooldridge::cps78_85
 
 
-## ----Aufgabe 4-----------------------------------------
+## ----Aufgabe 4------------------------------------
 data$wage <- exp(data$lwage)
 
 
-## ----Aufgabe 6-----------------------------------------
+## ----Aufgabe 6------------------------------------
 str(data)
 
 plot(
@@ -27,16 +27,16 @@ legend(
   "topleft", 
   legend = c("1978", "1985"), 
   col = c(1:2), 
-   pch = "o"
+  pch = "o"
 )
 
 
-## ----Aufgabe 7-----------------------------------------
+## ----Aufgabe 7------------------------------------
 model <- lm(formula = wage ~ educ, data = data)
 summary(model)
 
 
-## ----Aufgabe 8-----------------------------------------
+## ----Aufgabe 8------------------------------------
 plot(
   model$fitted, model$residuals,
   xlab = "angepasste Werte",
@@ -48,7 +48,7 @@ white <- lm(formula = I(model$residuals^2) ~ model$fitted + I(model$fitted^2))
 summary(white)
 
 
-## ----Aufgabe 10----------------------------------------
+## ----Aufgabe 10-----------------------------------
 model_log <- lm(formula = lwage ~ educ, data = data)
 summary(model_log)
 
@@ -66,14 +66,44 @@ white <- lm(formula = I(model_log$residuals^2) ~ model_log$fitted + I(model_log$
 summary(white)
 
 
-## ----Aufgabe 11----------------------------------------
+## ----Aufgabe 11-----------------------------------
 model_log_dummy <- lm(formula = lwage ~ educ + y85, data = data)
 summary(model_log_dummy)
 
 
-## ----Aufgabe 12----------------------------------------
+## ----Aufgabe 12-----------------------------------
 model_log_dummy_interact <- lm(
   formula = lwage ~ educ + y85 + I(y85 * educ), data = data
 )
 summary(model_log_dummy_interact)
+
+
+## ----Aufgabe 13-----------------------------------
+model_pool <- lm(formula = lwage ~ educ, data = data)
+summary(model_pool)
+model_1978 <- lm(formula = lwage ~ educ, data = data, subset = (y85 == 0))
+summary(model_1978)
+model_1985 <- lm(formula = lwage ~ educ, data = data, subset = (y85 == 1))
+summary(model_1985)
+
+
+## ----Aufgabe 14-----------------------------------
+### Teststatistik berechnen
+SSR_pool <- sum(model_pool$residuals^2)
+SSR_1978 <- sum(model_1978$residuals^2)
+SSR_1985 <- sum(model_1985$residuals^2) 
+n_1978 <- nobs(model_1978)
+n_1985 <- nobs(model_1985)
+K <- 1
+F <- (SSR_pool - (SSR_1978 + SSR_1985)) / (SSR_1978 + SSR_1985) * 
+  (n_1978 + n_1985 - 2 * (K + 1)) / (K + 1)
+
+### Testdurchführung
+alpha <- 0.05
+quantil <- qf(1- alpha, K + 1, n_1978 + n_1985 - 2 * (K + 1))
+F > quantil
+
+### alternativ mit p-value
+pvalue <- 1 - pf(F, K + 1, n_1978 + n_1985 - 2 * (K + 1))
+pvalue > alpha
 
